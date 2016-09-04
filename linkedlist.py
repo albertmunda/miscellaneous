5fb201084ea4644cb3b16f3da3b9996488a91640
Albert Mundu <albert.adel007@gmail.com>
2016-09-04 22:49:48 +0530

    Add files via upload

---
+++ linkedlist.py
@@ -0,0 +1,33 @@
+class Node(object):
+    def __init__(self,data=None,next_node=None):
+        self.data=data
+        self.next_node=next_node
+
+class LinkedList(object):
+    def __init__(self,head=None,prev=None):
+        self.head=head
+        self.prev=None
+
+    def insert(self,data):
+        if(self.head==None):
+            new_node=Node(data)
+            new_node.next_node=None
+            self.head=new_node
+            self.prev=new_node
+        else:
+            new_node=Node(data)
+            new_node.next_node=None
+            self.prev.next_node=new_node
+            self.prev=new_node
+
+    def delete(self):
+        self.head=self.head.next_node
+
+    def display(self):
+        tmp=self.head
+        while tmp!=None:
+            print tmp.data,
+            tmp=tmp.next_node
+        print ""
+        
+        
